1a42b4ce141b466a968336eedc42511b0e3ea9ad
Andrés Arias Medina <77759820+AndresAriasM@users.noreply.github.com>
2023-05-05 00:18:24 -0500

    Add files via upload

---
+++ ProgramaBeneficiariosMongo/Modelos/Municipio.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ProgramaBeneficiariosMongo.Modelos
+{
+    public class Municipio
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("ID_Municipio")]
+        public int ID_Municipio { get; set; }
+
+        [BsonElement("Nombre_Municipio")]
+        public string Nombre_Municipio { get; set; }
+
+        [BsonElement("Departamento")]
+        public int Departamento { get; set; }
+
+        public Municipio()
+        {
+            Id = string.Empty;
+            ID_Municipio = 0;
+            Nombre_Municipio = string.Empty;
+            Departamento = 0;
+        }
+    }
+}
